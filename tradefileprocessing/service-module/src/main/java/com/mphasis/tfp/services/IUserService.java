da74b8af3d1a52265b346d7c94995d4b6ae87d88
sushanthmph <sushanth.hp@mphasis.com>
2026-05-27 16:41:18 +0530

    Delete tradefileprocessing directory

--- tradefileprocessing/service-module/src/main/java/com/mphasis/tfp/services/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.mphasis.tfp.services;
-
-import com.mphasis.tfp.dto.LoginRequestDTO;
-import com.mphasis.tfp.dto.UserDetailsDTO;
-
-public interface IUserService {
-    String register(UserDetailsDTO request);
-    String login(LoginRequestDTO request);
-}
